bc9a404aecb9368c8877ccde1c1a0bfa4ea952d5
misticslippers <eternal.martyr13579@gmail.com>
2026-02-12 11:51:28 +0300

    Initial commit

---
+++ src/main/java/me/ifmo/lab1_business_logic_of_software_systems/Application.java
@@ -0,0 +1,13 @@
+package me.ifmo.lab1_business_logic_of_software_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1BusinessLogicOfSoftwareSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1BusinessLogicOfSoftwareSystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ifmo/lab1_business_logic_of_software_systems/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ifmo.lab1_business_logic_of_software_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1BusinessLogicOfSoftwareSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
