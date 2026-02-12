58fb68c2ad9c3bec555d54f0663bb6cdcf3393b6
misticslippers <eternal.martyr13579@gmail.com>
2026-02-12 18:59:40 +0300

    add CourseRepository

---
+++ src/main/java/me/ifmo/backend/repositories/CourseRepository.java
@@ -0,0 +1,19 @@
+package me.ifmo.backend.repositories;
+
+import me.ifmo.backend.entities.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    @Query("""
+           SELECT c FROM Course c
+           WHERE c.active = true
+           ORDER BY c.id ASC
+           """)
+    List<Course> findActiveCoursesOrdered();
+}
